eaa8a4dcd42c368dd3060c303cf72cf400c5b0a4
Horuz <ortizgd30@gmail.com>
2023-01-30 18:51:21 -0600

    avanzazmos con los inputs y una concatenacion

--- IMC.py
+++ IMC.py
@@ -4,4 +4,14 @@
 
 #Que datos vamos a pedir? 
 
+#El programa no puede permitir que ningún dato quede vacío, además de asegurarse de que en los campos de edad, peso y estatura el usuario introduzca una cifra. Todo esto antes de proceder con el cálculo del IMC siguiendo la fórmula:
+
+
 nombre = input("Escribe tu nombre: ")
+edad = int(input("Escribre tu edad: "))
+peso = int(input("Escribe tu peso: "))
+estatura = float(input("Escribe tu estatura: "))
+
+#ejemplo de concateacion
+print(nombre + " tu edad es " + str(edad) + "años")
+
