ee3ed7957bfb90a2470f2386ee7f07924fc6d9c9
Horuz <ortizgd30@gmail.com>
2023-01-30 18:30:43 -0600

    empezando el proyecto

---
+++ IMC.py
@@ -0,0 +1,3 @@
+#Aqui llevaremos acabo el primer proyecto con python
+#tenemos que hacer unos inputs para un calculo del imc 
+#la formula del IMC es =  Peso / estatura2   -> Peso sobre estatura al cuadrado
